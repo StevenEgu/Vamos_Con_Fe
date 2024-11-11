d50bca7c66113da08258be0da71c291ac8f873f9
angi666 <angela.flor.horna@estudiante.epsum.school>
2024-11-11 23:45:28 +0100

    npc_prueba

--- Proyecto_Juego/Assets/SCRIPTS/NPC.cs
+++ Proyecto_Juego/Assets/SCRIPTS/NPC.cs
@@ -1,41 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
     public GameObject dialoguePanel;
-    public Text dialogueText;
+    public TMP_Text dialogueText;
     public string[] dialogue;
     private int index;
 
-    public float wordSpeed;
+    public GameObject contButton;
+    public float wordSpeed = 0.05f;
     public bool playerIsClose;
-
-
 
     void Update()
     {
+        if (dialoguePanel == null || dialogueText == null || contButton == null)
+        {
+            Debug.LogError("Asegúrate de que todos los elementos están asignados en el Inspector.");
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
-
             if (dialoguePanel.activeInHierarchy)
             {
                 zeroText();
             }
             else
-            { 
+            {
                 dialoguePanel.SetActive(true);
                 StartCoroutine(Typing());
-            
             }
+        }
 
-        
+        if (dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
         }
     }
 
-    public void zeroText() 
+    public void zeroText()
     {
         dialogueText.text = "";
         index = 0;
@@ -44,15 +51,18 @@ public class NPC : MonoBehaviour
 
     IEnumerator Typing()
     {
-        foreach(char letter in dialogue[index].ToCharArray())
+        dialogueText.text = ""; // Reinicia el texto
+        foreach (char letter in dialogue[index].ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(wordSpeed);
         }
-
     }
+
     public void NextLine()
     {
+        contButton.SetActive(false);
+
         if (index < dialogue.Length - 1)
         {
             index++;
@@ -64,13 +74,15 @@ public class NPC : MonoBehaviour
             zeroText();
         }
     }
-    private void OnTriggerEnter2D(Collider2D other) 
-    { 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.CompareTag("Player"))
         {
             playerIsClose = true;
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
