2bab285d0a466b1fd84adf8a2ecacf07470d88a9
angi666 <angela.flor.horna@estudiante.epsum.school>
2024-11-14 01:58:01 +0100

    caca

--- Proyecto_Juego/Assets/SCRIPTS/InventoryItem.cs
+++ Proyecto_Juego/Assets/SCRIPTS/InventoryItem.cs
@@ -13,6 +13,7 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public void OnBeginDrag(PointerEventData eventData) {
         image.raycastTarget = false;
         parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
     }
 
     public void OnDrag(PointerEventData eventData) { 
@@ -21,6 +22,7 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     public void OnEndDrag(PointerEventData eventData) {
         image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
     }
        
 }
---
+++ Proyecto_Juego/Assets/SCRIPTS/Item.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Scriptable object/Item")]
+
+public class Item : ScriptableObject
+{ 
+    public TileBase tile;
+    public Sprite image;
+    public ItemType type;
+    public ActionType actionType;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+
+    [Header("Only gameplay")]
+
+    [Header("Only UI")]
+
+    [Header("Both")]
+}
+
+public enum ItemType
+{
+    BuildingBlock,
+    Tool
+}
+
+public enum ActionType
+{
+    Dig,
+    Mine
+}
