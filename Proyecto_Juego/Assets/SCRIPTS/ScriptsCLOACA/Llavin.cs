7bb94fa8b2effca2b81d74acae6c31aa0cf1b5dd
SantiagoAlejc <santiago.alejandro.hernandez@estudiante.epsum.school>
2024-11-19 11:17:27 +0100

    Cambioscloaca

--- Proyecto_Juego/Assets/SCRIPTS/ScriptsCLOACA/Llavin.cs
+++ Proyecto_Juego/Assets/SCRIPTS/ScriptsCLOACA/Llavin.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class llave : MonoBehaviour
+public class Llavin : MonoBehaviour
 {
     public GameObject Objetollave;
     public GameObject ColliderPuerta;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.CompareTag("Player"))
         {
-            ColliderPuerta.gameObject.SetActive(false);
+            ColliderPuerta.gameObject.SetActive(true);
             Destroy(Objetollave);
         }
     }
