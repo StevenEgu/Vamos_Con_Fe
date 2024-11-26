57e4711ab8899c1d8ac79bf5950d2a145045f3aa
SantiagoAlejc <santiago.alejandro.hernandez@estudiante.epsum.school>
2024-11-26 10:00:03 +0100

    fdfd

--- Proyecto_Juego/Assets/SCRIPTS/ScriptsCLOACA/PuertaVuelta.cs
+++ Proyecto_Juego/Assets/SCRIPTS/ScriptsCLOACA/PuertaVuelta.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PuertaVuelta : MonoBehaviour
 {
+    /* void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.CompareTag("Player"))
+         {
+             int nivelActual = SceneManager.GetActiveScene().buildIndex;
+             SceneManager.LoadScene(nivelActual - 4);
+         }
+     }*/
+
+    public string nombre;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            int nivelActual = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(nivelActual - 4);
+           
+            SceneManager.LoadScene(nombre);
         }
     }
 }
