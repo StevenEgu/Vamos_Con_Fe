0399507aed240e7f9c4850206e121a62564f7010
StevenEgu <steven.alfredo.eguizabal@estudiante.epsum.school>
2025-02-17 13:35:24 +0100

    YA

--- Proyecto_Juego/Assets/Asset/Nivel4/Scrips/EnemyMoveAway.cs
+++ Proyecto_Juego/Assets/Asset/Nivel4/Scrips/EnemyMoveAway.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;  // Importa el espacio de nombres para Light2D
 
-public class EnemyMoveWithAnimation : MonoBehaviour
+public class EnemyMoveAway : MonoBehaviour
 {
     public Animator enemyAnimator;  // El Animator del enemigo
     public string animationTriggerName = "Leave";  // Nombre del trigger de animación en el Animator
     public float moveSpeed = 5f;   // Velocidad de movimiento
     public Vector3 moveDirection = Vector3.left;  // Dirección en la que el enemigo se moverá (izquierda o derecha)
+
+    public Light2D enemyLight;  // Referencia a la luz 2D del enemigo
 
     private bool isMoving = false; // Para saber si el enemigo está en movimiento
 
@@ -17,6 +20,16 @@ public class EnemyMoveWithAnimation : MonoBehaviour
             // Activamos el trigger de la animación
             enemyAnimator.SetTrigger(animationTriggerName);
             isMoving = true;  // Iniciar movimiento
+
+            // Activar la luz 2D
+            if (enemyLight != null)
+            {
+                enemyLight.enabled = true;  // Encender la luz 2D
+            }
+            else
+            {
+                Debug.LogWarning("No se ha asignado la luz 2D al enemigo.");
+            }
         }
         else
         {
